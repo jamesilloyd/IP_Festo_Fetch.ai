0daad1ab282c3ef9891183b131b9da3feea60d89
James Lloyd <jijlloyd98@gmail.com>
2021-02-03 14:46:48 +0000

    first model added

--- MESA Models/first_model.py
+++ MESA Models/first_model.py
@@ -7,26 +7,36 @@ from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import ChartModule
 
-
 import matplotlib.pyplot as plt 
 import numpy as np
 
 
-# def compute_gini(model):
-#     agent_wealths = [agent.wealth for agent in model.schedule.agents]
-#     x = sorted(agent_wealths)
-#     N = model.num_agents
-#     B = sum(xi * (N-i) for i,xi in enumerate(x)) / (N*sum(x))
-#     return (1 + (1/N) - 2*B)
+def machine_utilisation(model):
+    
+    total_time_working = 0
+    total_time_free = 0
+    for agent in model.schedule.agents:
+        if(agent.agentType == 'machine'):
+            print(agent.timeFree)
+            print(agent.timeWorking)
+            total_time_free += agent.timeFree
+            total_time_working += agent.timeWorking
+
+    if(total_time_free != 0 and total_time_working != 0):
+        utilisation = total_time_working / (total_time_working + total_time_free)
+    else:
+        utilisation = 0
+    
+    return utilisation
 
 
 class OrderAgent(Agent):
 
     agentType = 'order'
     """An agent with fixed initial wealth."""
-    def __init__(self, unique_id, model):
+    def __init__(self, unique_id, model,operations):
         super().__init__(unique_id, model)
-        self.operations = ['CNC','3D','IM','3D','IM','3D','IM','CNC']
+        self.operations = operations
         self.lookingForResource = True
         self.completed = False
 
@@ -39,7 +49,7 @@ class OrderAgent(Agent):
             if(self.lookingForResource):
                 print('looking for resource')
                 for agent in self.model.schedule.agents:
-                    if(agent.unique_id  == 1):
+                    if(agent.agentType == 'scheduler'):
                         agent.orders.append(self)
                         self.model.grid.move_agent(self,agent.coordinates)
 
@@ -78,22 +88,24 @@ class MachineAgent(Agent):
         self.inProgress = False
         self.timeLeftOnOperation = 0
         self.coordinates = coordinates
+        self.timeFree = 0
+        self.timeWorking = 0
         self.order = OrderAgent
 
     def step(self):
         if(self.inProgress):
+            self.timeWorking += 1
             print(self.timeLeftOnOperation)
             self.timeLeftOnOperation -= 1
             if(self.timeLeftOnOperation == 0):
                 print('Order finished')
-
                 self.order.operations.pop(0)
                 self.order.lookingForResource = True
                 self.inProgress = False
+        else:
+            self.timeFree += 1
 
                 
-
-
 
 
 class ScheduleAgent(Agent):
@@ -108,41 +120,61 @@ class ScheduleAgent(Agent):
 
     def step(self):
         for nextAgent in self.orders:
-            print('New agent found')
+            foundMachine = False
+            print('Agent {0} found'.format(nextAgent.unique_id))
             if not nextAgent.operations:
+                print('New agent is completed')
                 nextAgent.completed = True
                 self.model.grid.move_agent(nextAgent,(0,0))
+                self.orders.remove(nextAgent)
             else:
+                print('New agent is looking for {0}'.format(nextAgent.operations[0]))
                 for agent in self.model.schedule.agents:
-                    if(agent.unique_id > 2):
+                    if(agent.agentType == 'machine'and not foundMachine):
                         if(agent.typeOfOperation == nextAgent.operations[0]):
                             print('Found matching machine')
                             # Move agent to this machine and start the operation
-                            self.model.grid.move_agent(nextAgent,agent.coordinates)
-                            agent.timeLeftOnOperation = agent.timeToComplete
-                            agent.inProgress = True
-                            agent.order = nextAgent
-                            self.orders.clear()
+                            if not agent.inProgress:
+                                print("Machine is free")
+                                self.model.grid.move_agent(nextAgent,agent.coordinates)
+                                agent.timeLeftOnOperation = agent.timeToComplete
+                                agent.inProgress = True
+                                agent.order = nextAgent
+                                self.orders.remove(nextAgent)
+                                foundMachine = True
+                            else:
+                                print('Machine is busy, waiting till free')
                         
 
 
         
 
-class MoneyModel(Model):
+class MyModel(Model):
     """A model with some number of agents."""
     def __init__(self, width, height):
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
 
-    
         scheduleAgent = ScheduleAgent(1,self,(5,0))
         self.schedule.add(scheduleAgent)
         self.grid.place_agent(scheduleAgent,(5,0))
 
-        orderAgent = OrderAgent(2,self)
+        orderAgent = OrderAgent(2,self, ['CNC','3D','CNC'])
         self.schedule.add(orderAgent)
-        self.grid.place_agent(orderAgent,(0,5))
+        self.grid.place_agent(orderAgent,(0,1))
+
+        orderAgent = OrderAgent(6,self, ['IM'])
+        self.schedule.add(orderAgent)
+        self.grid.place_agent(orderAgent,(0,2))
+
+        orderAgent = OrderAgent(7,self,['CNC','IM','CNC','IM','3D'])
+        self.schedule.add(orderAgent)
+        self.grid.place_agent(orderAgent,(0,3))
+
+        orderAgent = OrderAgent(8,self,['CNC','IM','3D','3D'])
+        self.schedule.add(orderAgent)
+        self.grid.place_agent(orderAgent,(0,4))
 
         
         machineAgent1 = MachineAgent(3,self,'CNC',4,(8,1))
@@ -153,9 +185,17 @@ class MoneyModel(Model):
         self.schedule.add(machineAgent2)
         self.grid.place_agent(machineAgent2,(8,4))
 
+        machineAgent2 = MachineAgent(9,self,'3D',8,(12,4))
+        self.schedule.add(machineAgent2)
+        self.grid.place_agent(machineAgent2,(12,4))
+
         machineAgent3 = MachineAgent(5,self,'IM',7,(8,8))
         self.schedule.add(machineAgent3)
         self.grid.place_agent(machineAgent3,(8,8))
+
+        machineAgent3 = MachineAgent(10,self,'IM',7,(12,8))
+        self.schedule.add(machineAgent3)
+        self.grid.place_agent(machineAgent3,(12,8))
 
 
         # # Create agents
@@ -167,17 +207,16 @@ class MoneyModel(Model):
         #     y = self.random.randrange(self.grid.height)
         #     self.grid.place_agent(a, (x, y))
 
-        # self.datacollector = DataCollector(
-        #     model_reporters = {"Gini":compute_gini},
-        #     agent_reporters= {"Wealth":"wealth"}
-        # )
+        self.datacollector = DataCollector(
+            model_reporters = {"Utilisation":machine_utilisation},
+            # agent_reporters = {"Wealth":"wealth"}
+        )
     
     def step(self):
         '''Advance the model by one step.'''
-        # self.datacollector.collect(self)
+        self.datacollector.collect(self)
         self.schedule.step()
         
-
 
 
 '''HOW TO VISUALISE THE MODEL'''
@@ -186,7 +225,7 @@ def agent_portrayal(agent):
         portrayal = {"Shape": "circle",
                 "Color": 'green' if agent.completed else 'blue',
                 "Filled": "true",
-                'text_color':'black',
+                'text_color':'white',
                 "Layer": 0,
                 'text': str(agent.unique_id),
                 "r": 0.3 }
@@ -210,11 +249,13 @@ def agent_portrayal(agent):
     return portrayal
     
 
-grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
-
-server = ModularServer(MoneyModel,[grid],'DM',{'width':10,'height':10})
+grid = CanvasGrid(agent_portrayal, 20, 20, 500, 500)
+chart = ChartModule([{'Label':'Utilisation',"Color":'Black'}],data_collector_name='datacollector')
+server = ModularServer(MyModel,[grid,chart],'DM',{'width':20,'height':20})
 server.port = 8521
 server.launch()
+
+
 
 
 '''HOW TO RUN A BATCH'''
@@ -238,7 +279,6 @@ server.launch()
 # plt.show()
 
 '''HOW TO SHOW THE DATA FROM DATACOLLECTOR'''
-
 # model = MoneyModel(50,10,10)
 # for i in range(100):
 #     model.step()
